74b4b341fb05a6ce8638402e01760b54e6ab2006
wolfram74 <phaugen@gmail.com>
2023-05-05 10:52:36 -0500

    testing read functionality

---
+++ src/main.js
@@ -0,0 +1,36 @@
+let sensors = {
+    'acc':{},
+    'grav':{},
+    'gyro':{},
+}
+
+function start_loop(){
+    console.log('go time')
+    sensors.acc.output = new LinearAccelerationSensor({frequency:30})
+    sensors.acc.log = []
+    sensors.acc.output.addEventListener('reading', (e)=>{
+        var entry = {}
+        entry.x = sensors.acc.output.x
+        entry.y = sensors.acc.output.y
+        entry.z = sensors.acc.output.z
+        entry.t = sensors.acc.output.timestamp
+        sensors.acc.log.push(entry)
+    })
+    sensors.acc.display =[
+        document.getElementById('acc_x'),
+        document.getElementById('acc_y'),
+        document.getElementById('acc_z'),
+        ]
+    setInterval(updateFields, 100)
+}
+
+function updateFields(){
+    console.log('updating')
+    let last_acc_entry = sensors.acc.log.length-1
+    console.log(last_acc_entry)
+    let acc_read = sensors.acc.log[last_acc_entry]
+    let acc_vals = [acc_read.x, acc_read.y, acc_read.y]
+    for (var i = 0; i <= 3; i++) {
+        sensors.acc.display[i].innerText = `${acc_vals[i]}`
+    }
+}
